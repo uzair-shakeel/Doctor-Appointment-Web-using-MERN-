1b2a50f600469ba641871aa1a5e197fce9fe71dc
uzair-shakeel <uzairshakeel00000@gmail.com>
2024-02-17 01:19:42 +0500

    .

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 import { AuthContext } from "../context/authContext";
-import BASE_URL from "../config";
+import { BASE_URL } from "../config";
 
 const Login = () => {
   const navigate = useNavigate();
